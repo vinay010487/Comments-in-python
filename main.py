536eba896b60e237f53aea36581dbce1ec50a62a
VinayRVishwakarma <36599592+vinay010487@users.noreply.github.com>
2020-12-26 14:17:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+
+
+#below Artist represent name of the artist 
+Artist = "Marshmello and Anne-Marie"
+
+#Genre of the song is pop 
+Genre = "pop"
+
+#below variable  represent song Duration in minute
+Duration_in_minute = 3.26
+
+#since its a english language 
+Language = 'English'
+
+#below variable represent number of the artist 
+Number_of_artist = 2
+
+#below lyrics variable represent Lyrics of the song 
+
+'''
+(Multi-Line Comment)
+You say you love me, I say you crazy
+We're nothing more than friends
+You're not my lover, more like a brother
+I known you since we were like ten, yeah 
+
+'''
+lyrics = 'You say you love me, I say you crazy'
+
+print(Artist)
+print(Genre)
+print(Duration_in_minute)
+print(Language)
+print(Number_of_artist)
+print(lyrics)
+
+
+
